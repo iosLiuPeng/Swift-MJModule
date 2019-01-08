20d73cd430f4e5f088b15189f8f7b489c43226f8
iosLiuPeng <392009255@qq.com>
2019-01-08 17:54:32 +0800

    MJAlert 默认实现 自测完成

--- Classes/ModuleAlert.swift
+++ Classes/ModuleAlert.swift
@@ -14,18 +14,186 @@ public let TheAlert = getTheModule("Alert") as! ModuleAlert.Type
 public let TheAlert = getTheModule("Alert", DefaultAlert.self) as! ModuleAlert.Type
 #endif
 
+/// 按钮类别
+public enum AlertInfoKey: String {
+    case title
+    case message
+    case cancel
+    case confirm
+    case destructive
+}
+
+// MARK: - ModuleAlert
 public protocol ModuleAlert: ModuleProtocol {
+    /// Alert点击回调
+    typealias AlertCompletion = (Int) -> Void
+
     
+    /// 显示一个弹框，自带确定按钮
+    static func showAlert(title: String?, message: String?, completion: AlertCompletion?)
+    
+    /// 显示一个弹框，自己处理按钮本地化
+    static func showAlert(title: String?, message: String?, cancel: String?, confirm: String?, completion: AlertCompletion?)
+    
+    /// 显示一个弹框，支持多个按钮，自己处理按钮本地化
+    static func showAlert(info: [AlertInfoKey: String], otherButtons: String..., completion: AlertCompletion?)
+    
+    
+    /// 显示一个ActionSheet，自带确定按钮
+    static func showActionSheet(title: String?, message: String?, onView: UIView, completion: AlertCompletion?)
+    
+    /// 显示一个ActionSheet，自己处理按钮本地化
+    static func showActionSheet(title: String?, message: String?, cancel: String?, confirm: String?, onView: UIView, completion: AlertCompletion?)
+    
+    /// 显示一个ActionSheet，支持多个按钮，自己处理按钮本地化
+    static func showActionSheet(info: [AlertInfoKey: String], otherButtons: String..., onView: UIView, completion: AlertCompletion?)
 }
 
 extension ModuleAlert {
     
 }
 
+
 // MARK: - DefaultAlert
 #if !MODULE_ALERT
 public class DefaultAlert: ModuleAlert {
+
+    /// 显示一个弹框，自带确定按钮
+    public static func showAlert(title: String?, message: String?, completion: AlertCompletion?) {
+        var info = [AlertInfoKey: String]()
+        info[.title] = title
+        info[.message] = message
+        
+        show(.alert, info: info, otherButtons: nil, onView: nil, completion: completion)
+    }
+    
+    /// 显示一个弹框，自己处理按钮本地化
+    public static func showAlert(title: String?, message: String?, cancel: String?, confirm: String?, completion: AlertCompletion?) {
+        var info = [AlertInfoKey: String]()
+        info[.title] = title
+        info[.message] = message
+        info[.cancel] = cancel
+        info[.confirm] = confirm
+        
+        show(.alert, info: info, otherButtons: nil, onView: nil, completion: completion)
+    }
+    
+    /// 显示一个弹框，支持多个按钮，自己处理按钮本地化
+    public static func showAlert(info: [AlertInfoKey: String], otherButtons: String..., completion: AlertCompletion?) {
+        show(.alert, info: info, otherButtons: otherButtons, onView: nil, completion: completion)
+    }
+    
+
+    /// 显示一个ActionSheet，自带确定按钮
+    public static func showActionSheet(title: String?, message: String?, onView: UIView, completion: AlertCompletion?) {
+        var info = [AlertInfoKey: String]()
+        info[.title] = title
+        info[.message] = message
+        
+        show(.actionSheet, info: info, otherButtons: nil, onView: onView, completion: completion)
+    }
+    
+    /// 显示一个ActionSheet，自己处理按钮本地化
+    public static func showActionSheet(title: String?, message: String?, cancel: String?, confirm: String?, onView: UIView, completion: AlertCompletion?) {
+        var info = [AlertInfoKey: String]()
+        info[.title] = title
+        info[.message] = message
+        info[.cancel] = cancel
+        info[.confirm] = confirm
+        
+        show(.actionSheet, info: info, otherButtons: nil, onView: onView, completion: completion)
+    }
+    
+    /// 显示一个ActionSheet，支持多个按钮，自己处理按钮本地化
+    public static func showActionSheet(info: [AlertInfoKey: String], otherButtons: String..., onView: UIView, completion: AlertCompletion?) {
+        show(.actionSheet, info: info, otherButtons: otherButtons, onView: onView, completion: completion)
+    }
     
     
+    /// 弹框
+    static func show(_ style: UIAlertController.Style, info: [AlertInfoKey: String], otherButtons: [String]?, onView: UIView?, completion: AlertCompletion?) {
+        let title = info[.title]
+        let message = info[.message]
+        
+        // 标题或内容必有一个
+        guard title != nil || message != nil else { return }
+        // 顶层控制器
+        guard let topVC = topViewController() else { return }
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: style)
+        var offset = 1;/// .default样式按钮的序号
+        
+        // 取消
+        if let cancel = info[.cancel] {
+            alert.addAction(UIAlertAction(title: cancel, style: .cancel, handler: { (AlertAction) in
+                completion?(0)
+            }))
+        }
+        
+        // 确定
+        if let confirm = info[.confirm] {
+            alert.addAction(UIAlertAction(title: confirm, style: .default, handler: { (AlertAction) in
+                completion?(1)
+            }))
+            offset += 1
+        }
+        
+        // 取消、确定按钮必须有一个（除了无按钮样式）
+        if alert.actions.count == 0 {
+            alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (AlertAction) in
+                completion?(1)
+            }))
+            offset += 1
+        }
+        
+        // 破坏性操作
+        if let destructive = info[.destructive] {
+            alert.addAction(UIAlertAction(title: destructive, style: .destructive, handler: { (AlertAction) in
+                completion?(-1)
+            }))
+        }
+        
+        // 其他按钮
+        if let otherButtons = otherButtons {
+            for title in otherButtons {
+                let index = offset
+                alert.addAction(UIAlertAction(title: title, style: .default, handler: { (AlertAction) in
+                    completion?(index)
+                }))
+                
+                offset += 1
+            }
+        }
+        
+        // ipad上 .actionSheet样式要特殊处理
+        if UIDevice.current.userInterfaceIdiom == .pad && style == .actionSheet {
+            // 必须要有一个按钮，不然点其他地方弹框消失了，window没消失
+            if  alert.actions.count == 0 {
+                alert.addAction(UIAlertAction(title: "Cancel" , style: .cancel, handler: { (AlertAction) in
+                    completion?(0)
+                }))
+            }
+            
+            // ipad上，必须要设置依靠的视图
+            if let onView = onView {
+                alert.popoverPresentationController?.sourceView = onView
+                alert.popoverPresentationController?.sourceRect = onView.bounds
+            }
+        }
+        
+        topVC.present(alert, animated: true, completion: nil)
+    }
+    
+    /// 顶部控制器
+    static func topViewController() -> UIViewController? {
+        guard let window = UIApplication.shared.delegate?.window, let aWindow = window, let rootVC = aWindow.rootViewController else { return nil }
+        
+        var top = rootVC
+        while top.presentedViewController != nil {
+            top = top.presentedViewController!
+        }
+        
+        return top
+    }
 }
 #endif
--- Swift-MJModule.podspec
+++ Swift-MJModule.podspec
@@ -1,7 +1,7 @@
 Pod::Spec.new do |s|
     s.name            = 'Swift-MJModule'
     s.module_name     = 'MJModule'
-
+    
     s.version         = '0.1.0'
     s.swift_version   = '4.2'
     s.summary         = 'This is a module management center in swift'
@@ -9,78 +9,80 @@ Pod::Spec.new do |s|
     s.license         = { :type => 'MIT', :file => 'LICENSE' }
     s.author          = { 'iosLiuPeng' => '392009255@qq.com' }
     s.source          = { :git => 'https://github.com/iosLiuPeng/Swift-MJModule.git', :tag => "v-#{s.version}" }
-    s.ios.deployment_target = '8.0'   
-
+    s.ios.deployment_target = '8.0'
+    
     s.default_subspec = 'Core'
     s.user_target_xcconfig = {
         'OTHER_SWIFT_FLAGS' => '-DMODULE'
     }
-  
+    
     s.preserve_path = 'ModuleConfig.swift'
+    
+    #包含所有模块
+#    s.source_files = 'Classes/**/*'
 
-
-  # 核心模块
-  s.subspec 'Core' do |ss|
-      ss.source_files = 'Classes/Core/*.swift'
-  end
-
-  s.subspec 'Networking' do |ss|
-      ss.source_files = 'Classes/ModuleNetworking.swift'
-      ss.dependency 'Swift-MJModule/Core'
-  end
-
-  s.subspec 'WebInterface' do |ss|
-      ss.source_files = 'Classes/ModuleWebInterface.swift'
-      ss.dependency 'Swift-MJModule/Core'
-  end
-
-  s.subspec 'Json' do |ss|
-      ss.source_files = 'Classes/ModuleJson.swift'
-      ss.dependency 'Swift-MJModule/Core'
-  end
-
-  s.subspec 'Localize' do |ss|
-      ss.source_files = 'Classes/ModuleLocalize.swift'
-      ss.dependency 'Swift-MJModule/Core'
-  end
-
-  s.subspec 'Resource' do |ss|
-      ss.source_files = 'Classes/ModuleResource.swift'
-      ss.dependency 'Swift-MJModule/Core'
-  end
-
-  s.subspec 'FilePath' do |ss|
-      ss.source_files = 'Classes/ModulePath.swift'
-      ss.dependency 'Swift-MJModule/Core'
-  end
-
-  s.subspec 'Keychain' do |ss|
-      ss.source_files = 'Classes/ModuleKeychain.swift'
-      ss.dependency 'Swift-MJModule/Core'
-  end
-
-  s.subspec 'Device' do |ss|
-      ss.source_files = 'Classes/ModuleDevice.swift'
-      ss.dependency 'Swift-MJModule/Core'
-  end
-
-  s.subspec 'Analyse' do |ss|
-      ss.source_files = 'Classes/ModuleAnalyse.swift'
-      ss.dependency 'Swift-MJModule/Core'
-  end
-
-  s.subspec 'Time' do |ss|
-      ss.source_files = 'Classes/ModuleTime.swift'
-      ss.dependency 'Swift-MJModule/Core'
-  end
-
-  s.subspec 'Alert' do |ss|
-      ss.source_files = 'Classes/ModuleAlert.swift'
-      ss.dependency 'Swift-MJModule/Core'
-  end
-
-  s.subspec 'IAP' do |ss|
-      ss.source_files = 'Classes/ModuleIAP.swift'
-      ss.dependency 'Swift-MJModule/Core'
-  end
+    # 核心模块
+    s.subspec 'Core' do |ss|
+        ss.source_files = 'Classes/Core/*.swift'
+    end
+    
+    s.subspec 'Networking' do |ss|
+        ss.source_files = 'Classes/ModuleNetworking.swift'
+        ss.dependency 'Swift-MJModule/Core'
+    end
+    
+    s.subspec 'WebInterface' do |ss|
+        ss.source_files = 'Classes/ModuleWebInterface.swift'
+        ss.dependency 'Swift-MJModule/Core'
+    end
+    
+    s.subspec 'Json' do |ss|
+        ss.source_files = 'Classes/ModuleJson.swift'
+        ss.dependency 'Swift-MJModule/Core'
+    end
+    
+    s.subspec 'Localize' do |ss|
+        ss.source_files = 'Classes/ModuleLocalize.swift'
+        ss.dependency 'Swift-MJModule/Core'
+    end
+    
+    s.subspec 'Resource' do |ss|
+        ss.source_files = 'Classes/ModuleResource.swift'
+        ss.dependency 'Swift-MJModule/Core'
+    end
+    
+    s.subspec 'FilePath' do |ss|
+        ss.source_files = 'Classes/ModulePath.swift'
+        ss.dependency 'Swift-MJModule/Core'
+    end
+    
+    s.subspec 'Keychain' do |ss|
+        ss.source_files = 'Classes/ModuleKeychain.swift'
+        ss.dependency 'Swift-MJModule/Core'
+    end
+    
+    s.subspec 'Device' do |ss|
+        ss.source_files = 'Classes/ModuleDevice.swift'
+        ss.dependency 'Swift-MJModule/Core'
+    end
+    
+    s.subspec 'Analyse' do |ss|
+        ss.source_files = 'Classes/ModuleAnalyse.swift'
+        ss.dependency 'Swift-MJModule/Core'
+    end
+    
+    s.subspec 'Time' do |ss|
+        ss.source_files = 'Classes/ModuleTime.swift'
+        ss.dependency 'Swift-MJModule/Core'
+    end
+    
+    s.subspec 'Alert' do |ss|
+        ss.source_files = 'Classes/ModuleAlert.swift'
+        ss.dependency 'Swift-MJModule/Core'
+    end
+    
+    s.subspec 'IAP' do |ss|
+        ss.source_files = 'Classes/ModuleIAP.swift'
+        ss.dependency 'Swift-MJModule/Core'
+    end
 end
